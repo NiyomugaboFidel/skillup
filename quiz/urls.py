0ca1a4db92cd6ee1e8d75e35b54d575b65d0e08a
NiyomugaboFidel <fidelniyomugabo67.com>
2025-05-09 09:08:03 +0200

    fix to

--- quiz/urls.py
+++ quiz/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('quiz/<int:quiz_id>/take/', views.take_quiz, name='take_quiz'),
     path('quiz/<int:quiz_id>/stats/', views.quiz_stats, name='quiz_stats'),
     path('attempt/<int:attempt_id>/results/', views.quiz_results, name='quiz_results'),
-     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    
 ]
